1bcee3e4a092260104435d4e65e54d663826fd4d
daffakzm08-sketch <daffakzm08@gmail.com>
2026-09-07 19:14:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,104 +1,65 @@
-// Import Module 
-const { makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require("baileys")
-const pino = require("pino")
-const chalk = require("chalk")
-const readline = require("readline")
-const { resolve } = require("path")
-const { version } = require("os")
+//import Module
+import { makeWASocket, useMultiFileAuthState } from "@whiskeysockets/baileys"
+import pino from "pino"
+import chalk from "chalk"
+import readline from "readline"
 
 // Metode Pairing
+// True = Pairing Code || False = Scan QR
 const usePairingCode = true
 
-// Promt Input Terminal
+// promt Input Terminal
 async function question(promt) {
-    process.stdout.write(promt)
-    const r1 = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    })
+   process.stdout.write(promt)
+   const r1 = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+   })
 
-    return new Promise((resolve) => r1.question("", (ans) => {
-        r1.close()
-        resolve(ans)
-    }))
-    
+   return new Promise((resolve) => r1.question("", (ans) => {
+      r1.close()
+      resolve(ans)
+   }))
 }
 
-async function connectToWhatsApp() {
-  const { state, saveCreds } = await useMultiFileAuthState('./LenwySesi')
-  
-  // Versi Terbaru
-  const { version, isLatest } = await fetchLatestBaileysVersion()
-  console.log(`Lenwy Using WA v${version.join('.')}, isLatest: ${isLatest}`)
+// Koneksi Whatsapp
+async function connectToWhatsApp(){
+   console.log(chalk.blue("😆  Memulai koneksi ke wahtsapp"))
 
-  const lenwy = makeWASocket({
-    logger: pino({ level: "silent" }),
-    printQRInTerminal: !usePairingCode,
-    auth: state,
-    browser: ['Ubuntu', 'Chrome', '20.0.04'],
-    version: version,
-    syncFullHistory: true,
-    generateHighQualityLinkPreview: true,
-    getMessage: async (key) => {
-      if (store) {
-        const msg = await store.loadMessage(key.remoteJid, key.id)
-        return msg?.message || undefined
-      }
-      return proto.Message.fromObject({})
-    }
-  })
+   // Meyimpan Sesi Login
+   // LenwySesi Menjadi Pyeimpanan Sesi Login
+   const { state, saveCreds } = await useMultiFileAuthState("./LenwySesi")
 
-  // Handle Pairing Code
-  if (usePairingCode && !lenwy.authState.creds.registered) {
-    try {
-      const phoneNumber = await question('😆 Masukan Nomor Yang Diawali Dengan 62 :\n')
+   // Membuat koneksi wahtsapp
+   const lenwy = makeWASocket({
+      logger: pino({ level: "silent"}),
+      printQRInTerminal: !usePairingCode,
+      auth: state, // pakai sesi yang ada
+      browser: ["Ubuntu", "Chrome", "20.0.04"], // simulasi browser
+   })
+
+   // metode Pairing Code
+   if (usePairingCode && !lenwy.authState.creds.registered) {
+      console.log(chalk.green("☺️  masukan nomor dengan awal 62"))
+      const phoneNumber = await question(">")
       const code = await lenwy.requestPairingCode(phoneNumber.trim())
-      console.log(`🎁 Pairing Code : ${code}`)
-    } catch (err) {
-      console.error('Failed to get pairing code:', err)
-    }
-  }
-    // Menyimpan Sesi Login
-    lenwy.ev.on("creds.update", saveCreds)
+      console.log(chalk.cyan(`😇pairin code : ${code}`))
+   }
 
-    // Informasi Koneksi
-    lenwy.ev.on("connection.update", (update) => {
-        const { connection, lastDisconnect } = update
-        if ( connection === "close") {
-            console.log(chalk.red("❌  Koneksi Terputus, Mencoba Menyambung Ulang"))
-            connectToWhatsApp()
-        } else if ( connection === "open") {
-            console.log(chalk.green("✔  Bot Berhasil Terhubung Ke WhatsApp"))
-        }
-    })
+   // menyimpan sesi Login
+   lenwy.ev.on("creds.update", saveCreds)
 
-    // Respon Pesan Masuk
-    lenwy.ev.on("messages.upsert", async (m) => {
-        const msg = m.messages[0]
-
-        if (!msg.message) return
-
-        const body = msg.message.conversation || msg.message.extendedTextMessage?.text || ""
-        const sender = msg.key.remoteJid
-        const pushname = msg.pushName || "Lenwy"
-
-        // Log Pesan Masuk Terminal
-        const listColor = ["red", "green", "yellow", "magenta", "cyan", "white", "blue"]
-        const randomColor = listColor[Math.floor(Math.random() * listColor.length)]
-
-        console.log(
-            chalk.yellow.bold("Credit : Lenwy"),
-            chalk.green.bold("[ WhatsApp ]"),
-            chalk[randomColor](pushname),
-            chalk[randomColor](" : "),
-            chalk.white(body)
-            
-        )
-
-        require("./lenwy")(lenwy, m)
-    })
-    
+   // Informasi koneksi
+   lenwy.ev.on("connection.update", (update) => {
+      const {connection, lastDisconnect } = update
+      if (connection === "close") {
+         console.log(chalk.red("❌ koneksi terputus, Mencoba menyambung ulang"))
+         connectToWhatsApp()
+      } else if (connection === "open") {
+         console.log(chalk.green("✔️ Berhasil terhubung ke whatsapp"))
+      }
+   })
 }
 
-// Jalankan Koneksi WhatsApp
+// Jalankan koneksi wahtsapp
 connectToWhatsApp()
